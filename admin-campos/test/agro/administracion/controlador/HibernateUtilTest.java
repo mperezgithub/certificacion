65bead2917ee619b6606c9d20479d6535395a3d0
Angel Mario Perez <amarioperez@gmail.com>
2018-12-17 01:04:24 -0300

    contiene el Caso de Uso 12 - Registrar Campos
    Certificacion de Instructores - Proyecto Administracion de Campos
    Caso de Uso 12 - Registrar Campos

---
+++ admin-campos/test/agro/administracion/controlador/HibernateUtilTest.java
@@ -0,0 +1,60 @@
+/*
+ * Proyecto Administracion de Campos - Agro SRL
+ * 
+ * 2018 Certificacion de Instructores - Plan 111mil
+ */
+package agro.administracion.controlador;
+
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author AMP <Angel Mario Perez>
+ */
+public class HibernateUtilTest {
+    
+    public HibernateUtilTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getSessionFactory method, of class HibernateUtil.
+     */
+    @Test
+    public void testGetSessionFactory() {
+        System.out.println("getSessionFactory");
+        SessionFactory result = HibernateUtil.getSessionFactory();
+        assertNotNull(result);
+    }
+
+    /**
+     * Test of shutdown method, of class HibernateUtil.
+     */
+    @Test
+    public void testShutdown() {
+        System.out.println("shutdown");
+        HibernateUtil.shutdown();
+        assertTrue(HibernateUtil.getSessionFactory().isClosed());
+    }
+}
